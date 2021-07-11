b94eb8897393a5f4d22d731b589d5f4c656721d3
MehakGautam <84184731+MehakGautam@users.noreply.github.com>
2021-07-11 21:10:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var dog,sadDog,happyDog;
+var foodS,foodStock;
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feed=createButton("Feed The Dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  })
+
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed :"+ lastFed%12 + "PM",350,30);
+  }else if(lastFed==0){
+    text("Last Feed : 12AM",350,30);
+  }else{
+    text("Lat Feed : "+ lastFed + "AM",350,30);
+  }
+
+  //function to read food Stock
+  foodS=data.val();
+
+  foodObj.updateFoodsStock(foodS);
+  drawSprites();
+}
+
